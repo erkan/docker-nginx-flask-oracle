9da47a04f943e2a23dce955995a9445731ae8da2
Erkan Lanpir <elanpir@gmail.com>
2020-10-11 00:49:43 +0300

    oracle client copied folder change

--- install-instantclient.sh
+++ install-instantclient.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-unzip instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
-unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+unzip /opt/data/instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+unzip /opt/data/instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
 mv /opt/oracle/instantclient_12_1 /opt/oracle/instantclient
 ln -s /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so
 ln -s /opt/oracle/instantclient/libocci.so.12.1 /opt/oracle/instantclient/libocci.so
