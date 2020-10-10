d70925cef56cf4c589aaee19fbd7016d50456959
Erkan Lanpir <elanpir@gmail.com>
2020-10-11 02:33:02 +0300

    uwsgi tests

--- app/main.py
+++ app/main.py
@@ -1,13 +1,4 @@
-from flask import Flask
+from wsgi import app
 
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Hello world!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+if __name__ == "__main__":
+    app.run()
