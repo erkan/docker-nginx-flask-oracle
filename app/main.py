a440a4cb3813d2948d57ac8a69edd6ed02ab6140
Erkan Lanpir <elanpir@gmail.com>
2020-10-11 01:04:38 +0300

    Changed test flusk app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-import sys
+from flask import Flask
 
 
-def application(env, start_response):
-    version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    message = "Hello World from a default Nginx uWSGI Python {} app in a Docker container (default)".format(
-        version
-    )
-    return [message.encode("utf-8")]
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
